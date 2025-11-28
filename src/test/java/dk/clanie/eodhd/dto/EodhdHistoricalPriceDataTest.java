d7675450115491c307af2d2d4860574dc3153a83
Claus Nielsen <clausn999@gmail.com>
2025-11-28 12:29:58 +0100

    Fix copyright notices

--- src/test/java/dk/clanie/eodhd/dto/EodhdHistoricalPriceDataTest.java
+++ src/test/java/dk/clanie/eodhd/dto/EodhdHistoricalPriceDataTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025, Claus Nielsen, clausn999@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package dk.clanie.eodhd.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,7 +30,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import dk.clanie.eodhd.dto.EodhdHistoricalPriceData;
 
 class EodhdHistoricalPriceDataTest {
-
 
 	@Test
 	void testDeserialization() throws IOException {
@@ -62,6 +78,5 @@ class EodhdHistoricalPriceDataTest {
 		assertThat(value2.getAdjustedClose()).isEqualTo(285.2);
 		assertThat(value2.getVolume()).isEqualTo(98765432);
 	}
-
 
 }
